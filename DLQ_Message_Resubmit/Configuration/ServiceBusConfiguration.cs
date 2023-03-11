84c1b14a68ecf397a6ada264a0b8999c1b3da996
Mike Williams <admin@mikewilliams.tech>
2023-03-11 09:45:54 -0800

    Implement IoC - Add ASB configuration to local.settings.json - Remove ASB configuration from code - Add secrets.json to hold ASB connection string - Add Startup class to implement IoC - Implement Options pattern to hold ASB configuration

---
+++ DLQ_Message_Resubmit/Configuration/ServiceBusConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace DLQ_Message_Resubmit.Configuration
+{
+    public class ServiceBusConfiguration
+    {
+        public string ConnectionString { get; set; }
+        public string TopicName { get; set; }
+        public string SubscriptionName { get; set; }
+    }
+}
